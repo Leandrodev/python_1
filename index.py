342d3eccb391e9fd1fa7b33e89512d04655e7a50
leandro-rodriguess <leandro.rbarbosa2905@gmmail.com>
2026-09-02 20:22:41 -0300

    primeira aula python

---
+++ index.py
@@ -0,0 +1,32 @@
+#correcao da primeira aula de python 
+
+#n1
+
+print(4 + 10 % 3)
+print(8 + 3 * 2)
+print(20 // 3 + 2)
+print(15 % 4 * 2)
+print(2 ** 3 + 4)
+print(2 ** 3 + 4)
+print(18 // 4 * 2)
+print(10 - 3 * 2 + 5)
+
+#n2
+
+print(5 + 2.5 * 2)
+print(10 / 4 + 3)
+print(7.5 + 2 * 3)
+print(20/ 4 + 2.5)
+print(10 // 3 + 1)
+print(2.5 ** 2 + 1)
+
+#n3
+
+print("python" + "3")#python3
+print("py" * 3) #pypypy
+print("10" + "5")#105
+print("A" * 4 + "B")#AAAB 
+print("python" + "!" * 3)# python!!!
+print("=" * 5 + "python" + "=" * 5)#=====python=====
+
+
